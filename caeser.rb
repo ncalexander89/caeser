4702003d4a352de0886c186d9bd1d35b21c87141
scoobymcdoobsville <78505801+scoobymcdoobsville@users.noreply.github.com>
2022-07-13 18:55:28 +1000

    Add files via upload

---
+++ caeser.rb
@@ -0,0 +1,20 @@
+def caeser_cipher (string, shift)
+  #change to array of ASCII    
+  num_array=string.split("").map { |letter| letter.ord} 
+  #cycle through each number and convert if meets requirement
+  char = num_array.map do |number| 
+    #check for if alphabet ASCII, add shift
+    if number.between?(65,90) || number.between?(97,122)
+    number = number+shift
+      #check for alphabet ASCII after shift, otherwise subtract 26
+      unless number.between?(65,90) || number.between?(97,122)
+      number=number-26
+      end
+    end
+    # change ASCII back to character
+    number.chr
+    end
+  #convert to string
+  char.join("")
+end
+puts caeser_cipher("What a string!",5)
