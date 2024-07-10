a6e3f071302b4d24edfb3797a9f9327da73f5ff4
Chris Alexander <nc.alexander89@gmail.com>
2024-07-10 12:02:39 +1000

    updated script for rspec

--- lib/caeser.rb
+++ lib/caeser.rb
@@ -1,14 +1,15 @@
-def caeser_cipher (string, shift)
-  #change to array of ASCII    
-  num_array=string.split("").map { |letter| letter.ord} 
-  #cycle through each number and convert if meets requirement
-  char = num_array.map do |number| 
+class Caeser
+  def caeser_cipher(string, shift)
+    #change to array of ASCII    
+    num_array=string.split("").map { |letter| letter.ord} 
+    #cycle through each number and convert if meets requirement
+    char = num_array.map do |number| 
     #check for if alphabet ASCII, add shift
     if number.between?(65,90) || number.between?(97,122)
-    number = number+shift
+      number = number+shift
       #check for alphabet ASCII after shift, otherwise subtract 26
       unless number.between?(65,90) || number.between?(97,122)
-      number=number-26
+        number=number-26
       end
     end
     # change ASCII back to character
@@ -16,5 +17,6 @@ def caeser_cipher (string, shift)
     end
   #convert to string
   char.join("")
+  end
 end
-puts caeser_cipher("What a string!",5)
+# puts caeser_cipher("What a string!",5)
