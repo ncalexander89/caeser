b974ba7849656d94a41d35a6bfd26eefd9cad1dc
Chris Alexander <nc.alexander89@gmail.com>
2024-07-26 14:03:42 +1000

    Tidy up code

--- lib/caeser.rb
+++ lib/caeser.rb
@@ -1,24 +1,26 @@
-class Caeser
+# frozen_string_literal: true
+
+class Caeser # rubocop:disable Style/Documentation
   def caeser_cipher(string, shift)
-    #change to array of ASCII    
-    num_array=string.split("").map { |letter| letter.ord} 
-    #cycle through each number and convert if meets requirement
-    char = num_array.map do |number| 
-    #check for if alphabet ASCII, add shift
-    if number.between?(65,90) || number.between?(97,122)
-      number = number+shift
-      #check for alphabet ASCII after shift, otherwise subtract 26
-      unless number.between?(65,90) || number.between?(97,122)
-        number=number-26
+    # change to array of ASCII
+    # &:ord is a shorthand for { |char| char.ord }.
+    # The ord method returns the ASCII value of the character. For example, 'h'.ord returns 104.
+    num_array = string.split('').map(&:ord)
+    # cycle through each number and convert if meets requirement
+    char = num_array.map do |number|
+      # check for if alphabet ASCII, add shift
+      if number.between?(65, 90) || number.between?(97, 122)
+        number += shift
+        # check for alphabet ASCII after shift, otherwise subtract 26
+        number -= 26 unless number.between?(65, 90) || number.between?(97, 122)
       end
+      # change ASCII back to character
+      number.chr
     end
-    # change ASCII back to character
-    number.chr
-    end
-  #convert to string
-  char.join("")
+    # convert to string
+    char.join('')
   end
 end
 
-caeser=Caeser.new
-p caeser.caeser_cipher("What a string!",-5)
+caeser = Caeser.new
+p caeser.caeser_cipher('What a string!', -5)
--- spec/caeser_spec.rb
+++ spec/caeser_spec.rb
@@ -1,17 +1,18 @@
-#spec/caeser_spec.rb
+# frozen_string_literal: true
+
+# spec/caeser_spec.rb
 
 require './lib/caeser'
 
 describe Caeser do
-    describe "#caeser_cipher" do
-      it "turns string into ASCII, adds positive shift, returns new string" do
-        caeser = Caeser.new
-        expect(caeser.caeser_cipher("What a string!",5)).to eql("Bmfy f xywnsl!")
-      end
-      it "turns string into ASCII, adds negative shift, returns new string" do
-        caeser = Caeser.new
-        expect(caeser.caeser_cipher("What a string!",-5)).to eql("RcBo B nomdib!")
-      end
+  describe '#caeser_cipher' do
+    it 'turns string into ASCII, adds positive shift, returns new string' do
+      caeser = Caeser.new
+      expect(caeser.caeser_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
+    end
+    it 'turns string into ASCII, adds negative shift, returns new string' do
+      caeser = Caeser.new
+      expect(caeser.caeser_cipher('What a string!', -5)).to eql('RcBo B nomdib!')
     end
   end
-  
+end
